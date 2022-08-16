e75285802c42b6155ccef63b131ac168626f97d8
Diego Cisneros <diego.cisneros.gp@gmail.com>
2022-08-16 11:01:08 -0500

    Agruegue el diseño del loguin y de los formularios para el registro de un usuario al igual que los archivos php para recibir los datos de los formularios para la insercion de datos en la BD

---
+++ views/scripts/AltaUsuario2.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+  </head>
+  <body>
+  <?php
+  
+  try
+  {
+    if (isset($_POST["correo"]) && $_POST["contra"])
+    { $correo = $_POST['correo'];
+      $contra = $_POST['contra'];
+        
+     if (!empty($contra) && !empty($correo))
+    {
+        
+    
+     $consulta1="SELECT correo FROM usuario WHERE correo='$correo'";
+     $buscarcorr = $query -> seleccionar($consulta1);
+     $cantCorr= COUNT($buscarcorr);
+    
+     if($cantCorr > 0)
+     {
+       ?>
+         <div class="alert alert-danger" role="alert">
+           El Correo Electronico ya esta en uso intenta con otro
+         </div>
+       <?php
+       
+       header("refresh:3; ../FormRegistroP1.php");
+     }
+      else 
+      {
+       
+     $hash=password_hash($contra,PASSWORD_DEFAULT);
+     $insertuser="INSERT INTO usuario(correo,contraseña,persona,rol) VALUES
+     ('$correo','$hash','$id_per',1)";
+     $insert->ejecuta($insertuser);
+      echo "<div class='alert alert-success' align='center' role='alert'>Usuario registrado correctamente!</div>";
+     header("refresh:3; ../FormLogin.php"); 
+      }}
+    }
+   
+  } 
+  
+  catch(PDOException $e)
+  {
+    echo "<div class='alert alert-danger' align='center' role='alert'>". $e->getMessage() . "</div>";
+  }
+?>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+  </body>
+</html>
